21c3da16c015e0b2a566476a34e70e78fbe28fb9
juan <juanjuangmo@gmail.com>
2025-08-04 17:20:28 +0200

    Enhance showSnackbar function to accept optional color parameter and improve text styling

--- lib/shared/functions/show_snackbar.dart
+++ lib/shared/functions/show_snackbar.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 
 
-void showSnackbar( BuildContext context, String message ) {
+void showSnackbar( BuildContext context, String message, [ Color? color ] ) {
+
+  final textStyle = Theme.of(context).textTheme.titleSmall;
+  
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
   ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text(message))
+    SnackBar(
+      content: Text(message, style: textStyle),
+      duration: const Duration(seconds: 3),
+      backgroundColor: color ?? Colors.red,
+    )
   );
 }
