b6a2f12556897229ab3634be0b3d36e72a662e3a
juan <juanjuangmo@gmail.com>
2025-08-05 18:52:09 +0200

    Add TaskResponse class and JSON mapping functions for task data handling

---
+++ lib/domain/infrastructure/mappers/task_mapper.dart
@@ -0,0 +1,50 @@
+// To parse this JSON data, do
+//
+//     final taskResponse = taskResponseFromMap(jsonString);
+
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+import 'package:todo_app/domain/entities/task.dart';
+
+TaskResponse taskResponseFromMap(String str) => TaskResponse.fromMap(json.decode(str));
+
+String taskResponseToMap(TaskResponse data) => json.encode(data.toMap());
+
+class TaskResponse {
+    int id;
+    String body;
+    TaskPriority priority;
+    DateTime taskDate;
+    bool completed;
+
+    TaskResponse({
+        required this.id,
+        required this.body,
+        required this.priority,
+        required this.taskDate,
+        required this.completed,
+    });
+
+    factory TaskResponse.fromMap(Map<String, dynamic> json) => TaskResponse(
+        id: json["id"],
+        body: json["body"],
+        // Convert the string priority to TaskPriority enum
+        priority: TaskPriority.values.firstWhere(
+          (e) => e.name.toUpperCase() == json["priority"].toUpperCase(),
+        ),
+        taskDate: DateTime.parse(json["taskDate"]),
+        completed: json["completed"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "body": body,
+        "priority": priority.name.toUpperCase(),
+        "taskDate": "${taskDate.year.toString().padLeft(4, '0')}-${taskDate.month.toString().padLeft(2, '0')}-${taskDate.day.toString().padLeft(2, '0')}",
+        "completed": completed,
+    };
+
+    // Convert DateTime to JSON format 'yyyy-MM-dd'
+    static String dateTimeToJsonFormat(DateTime date) => DateFormat('yyyy-MM-dd').format(date);
+}
