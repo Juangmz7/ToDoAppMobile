da630f343ced7d2dee60e3e53e4a59668b0db195
juan <juanjuangmo@gmail.com>
2025-08-05 17:13:17 +0200

    Add taskUrl getter to AppConfig for task API endpoint

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -5,4 +5,6 @@ class AppConfig {
   static String get apiUrl => dotenv.env['API_URL'] ?? 'http://localhost:8081';
 
   static String get authUrl => '$apiUrl/auth';
+
+  static String get taskUrl => '$apiUrl/api/tasks';
 }
