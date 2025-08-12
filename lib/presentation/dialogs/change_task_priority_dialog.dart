3967e75e606babeb4825f0ec750eae47b9aaabc6
juan <juanjuangmo@gmail.com>
2025-08-12 17:53:28 +0200

    Change task priority dialog background color changed

--- lib/presentation/dialogs/change_task_priority_dialog.dart
+++ lib/presentation/dialogs/change_task_priority_dialog.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:todo_app/config/config.dart';
 import 'package:todo_app/presentation/widgets/change_task_priority_form.dart';
 
 class ChangeTaskPriorityDialog {
@@ -19,7 +18,7 @@ class ChangeTaskPriorityDialog {
         return GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
           child: Dialog(
-            backgroundColor: AppTheme.taskPagesBackground,
+            backgroundColor: const Color.fromARGB(255, 29, 3, 100),
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 20),
               child: ChangeTaskPriorityForm(taskId: taskId)
